f3f4c24d23d7075c41580b4c2e7b853f73ba6fb8
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-01-10 12:36:30 -0300

    quiz react

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -4,6 +4,7 @@ import { QuizContext } from './context/quiz'
 
 import Welcome from './components/Welcome'
 import Question from './components/Question'
+import { GameOver } from './components/GameOver'
 
 import './App.css'
 
@@ -21,6 +22,7 @@ function App() {
         <h1>Quiz de Programação</h1>
         {quizState.gameStage === "Start" && <Welcome/>}
         {quizState.gameStage === "Playing" && <Question/>}
+        {quizState.gameStage === "End" && <GameOver/>}
       </div>
     </>
   )
--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -1,9 +1,20 @@
 import { useContext } from 'react'
 import { QuizContext } from '../context/quiz'
 
+import Option from './Option'
+
+import './Question.css'
+
 const Question = () => {
   const [quizState, dispatch] = useContext(QuizContext);
   const currentQuestion = quizState.questions[quizState.currentQuestion]
+
+  const onSelectOption = (option) => {
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: {answer: currentQuestion.answer, option}
+    });
+  }
 
   return (
     <div id='question'>
@@ -13,9 +24,20 @@ const Question = () => {
       <h2>{currentQuestion.question}</h2>
       <p>Pergunta atual</p>
       <div id='options-container'>
-        <p>Opções</p>
+        {currentQuestion.options.map((option) => (
+          <Option 
+            option={option} 
+            key={option} 
+            answer={currentQuestion.answer}
+            selectOption={() => onSelectOption(option)}
+          />
+        ))}
       </div>
-      <button>Continuar</button>
+      {quizState.answerSelected && (
+        <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
+          Continuar
+        </button>
+      )}
     </div>
   )
 }
